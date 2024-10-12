b21b32d5f9f8267cfb52a417f7960b20a007f7e3
up958769 <dsindilaisme@gmail.com>
2024-10-12 19:40:52 +0100

    Implemented daily SMA algorithm

--- src/data_collection.py
+++ src/data_collection.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 # Fetch data
 ticker = 'AAPL'
-data = yf.download(ticker, start="2020-01-01", end="2024-01-01")
+data = yf.download(ticker, start="2014-01-01", end="2024-01-01")
 
 data.ffill(inplace=True)  # Forward fill
 
--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 ticker = "AAPL"
 data_path = f"../data/{ticker}_historical_data.csv"
+save_path = f"../data/{ticker}_processed_data.csv"
 
 data = pd.read_csv(data_path)
 data = data[['Date', 'Close', 'Volume']]
 data.set_index('Date', inplace=True)
+data.to_csv(save_path)
 
 print(data.head())
