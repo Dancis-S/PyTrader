15bffa34e2e86eff61ce871acf9e365d1703909d
up958769 <dsindilaisme@gmail.com>
2024-10-28 00:51:11 +0000

    Corrections

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
+
 ticker = "AAPL"
 data_path = f"../data/{ticker}_historical_hourly_data.csv"
 save_path = f"../data/{ticker}_processed_hourly_data.csv"
 
 data = pd.read_csv(data_path)
+data['Datetime'] = pd.to_datetime(data['Datetime'])
 data = data[['Datetime', 'Close', 'Volume']]
 data.set_index('Datetime', inplace=True)
 data.to_csv(save_path)
