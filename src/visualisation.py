2611d7960451effb8a099a3e2c38dd617cf3c112
up958769 <dsindilaisme@gmail.com>
2024-11-03 23:43:54 +0000

    Added in requirements file for linter

--- src/visualisation.py
+++ src/visualisation.py
@@ -13,4 +13,10 @@ def plot_portfolio_value(data, strategy_name):
     plt.grid()
     plt.legend()
     plt.show()
+
+
+def plot_trade_points(data, stratergy_name):
+    """
     
+    """
+    pass
