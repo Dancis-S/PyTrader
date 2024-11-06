7a88331afd35f5287b1994bac3b3783ec25f4b3a
up958769 <dsindilaisme@gmail.com>
2024-11-06 23:37:28 +0000

    Added graph that plots buy/sell points on the stock price graph

--- src/visualisation.py
+++ src/visualisation.py
@@ -5,6 +5,7 @@ def plot_portfolio_value(data, strategy_name):
     Plots the value of the portfolio over time
 
     """
+    print("Hello")
     plt.figure(figsize=(12,6))
     plt.plot(data.index, data['Portfolio Value'], label="Portfolio Value")
     plt.xlabel("Date")
@@ -15,8 +16,26 @@ def plot_portfolio_value(data, strategy_name):
     plt.show()
 
 
-def plot_trade_points(data, stratergy_name):
+def plot_trade_points(data, strategy_name):
     """
+    Plot the stock price and the Buy/Sell points.
+    """
+    plt.figure(figsize=(12, 6))
+    plt.plot(data.index, data['Close'], label='Stock Price', color='black')
     
-    """
-    pass
+    # Plot buy signals
+    buys = data[data['Actions'] == 1]
+    plt.plot(buys.index, buys['Close'], '^', markersize=10, color='green', label='Buy Signal')
+    
+    # Plot sell signals
+    sells = data[data['Actions'] == -1]
+    plt.plot(sells.index, sells['Close'], 'v', markersize=10, color='red', label='Sell Signal')
+    
+    plt.xlabel('Date')
+    plt.ylabel('Stock Price ($)')
+    plt.title(f'Trade Signals - {strategy_name} Strategy')
+
+    plt.grid()
+    plt.legend()
+    plt.show()
+    
